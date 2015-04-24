82545c7257d329e87ec6a6fdf2278c94c1481dd9
Nate <Natejd04@gmail.com>
2015-04-23 20:13:42 -0700

    init

---
+++ db/schema.rb
@@ -0,0 +1,45 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150424030625) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "qbwc_jobs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "company",                          limit: 1000
+    t.string   "worker_class",                     limit: 100
+    t.boolean  "enabled",                                       default: false, null: false
+    t.integer  "request_index",                                 default: 0,     null: false
+    t.text     "requests"
+    t.boolean  "requests_provided_when_job_added",              default: false, null: false
+    t.text     "data"
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
+  end
+
+  create_table "qbwc_sessions", force: :cascade do |t|
+    t.string   "ticket"
+    t.string   "user"
+    t.string   "company",      limit: 1000
+    t.integer  "progress",                  default: 0,  null: false
+    t.string   "current_job"
+    t.string   "iterator_id"
+    t.string   "error",        limit: 1000
+    t.string   "pending_jobs", limit: 1000, default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+  end
+
+end
