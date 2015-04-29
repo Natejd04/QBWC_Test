2b36e31dafbe4454fb55e727e5c79a7d21c0ccc1
Nate <Natejd04@gmail.com>
2015-04-29 08:43:50 -0700

    Attempt 1 at Mod Worker

--- app/workers/customer_modify_worker.rb
+++ app/workers/customer_modify_worker.rb
@@ -1,43 +1,43 @@
-#require 'qbwc'
-#
-#class CustomerModifyWorker < QBWC::Worker
-#
-#    def requests(job)
-#        {
-#            :customer_mod_rq => {
-#                
-#                '<QBXML>
-#                   <QBXMLMsgsRq onError="continueOnError">
-#            <CustomerModRq>
-#            <CustomerMod>
-#            <ListID ></ListID>
-#            <EditSequence ></EditSequence>
-#            <BillAddress>
-#            <Addr1 ></Addr1>
-#            <Addr2 ></Addr2> 
-#            <City ></City>
-#            <State ></State>
-#            <PostalCode ></PostalCode>
-#            </BillAddress>
-#            </CustomerModRq>
-#            </CustomerMod>
-#                   </QBXMLMsgsRq>
-#
-#                    </QBXML>'
-#                
-#                    },
-#            
-#        }
-#    end
-#
-#    def handle_response(r, session, job, request, data)
-#        # handle_response will get customers in groups of 100. When this is 0, we're done.
-#        
-#      
-#    end
-#
-#    
-#end
+require 'qbwc'
+
+class CustomerModifyWorker < QBWC::Worker
+
+    def requests(job)
+        {
+            :customer_mod_rq => {
+                
+                '<QBXML>
+                   <QBXMLMsgsRq onError="continueOnError">
+            <CustomerModRq>
+            <CustomerMod>
+            <ListID >80000079</ListID>
+            <EditSequence >1576443701</EditSequence>
+            <BillAddress>
+            <Addr1 >1234 Seasme St</Addr1>
+            <Addr2 >Apt 103</Addr2> 
+            <City >Seattle</City>
+            <State >WA</State>
+            <PostalCode >98125</PostalCode>
+            </BillAddress>
+            </CustomerModRq>
+            </CustomerMod>
+                   </QBXMLMsgsRq>
+
+                    </QBXML>'
+                
+                    },
+            
+        }
+    end
+
+    def handle_response(r, session, job, request, data)
+        # handle_response will get customers in groups of 100. When this is 0, we're done.
+        Rails.logger.info("This completes the Customer Mod Worker")
+      
+    end
+
+    
+end
 #
 ## '<QBXML>
 ##
