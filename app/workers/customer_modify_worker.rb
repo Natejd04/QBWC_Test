4efab77daf8aef9e6e68e65f683c47304f0d526b
Nate <Natejd04@gmail.com>
2015-04-30 11:55:31 -0700

    went all static again

--- app/workers/customer_modify_worker.rb
+++ app/workers/customer_modify_worker.rb
@@ -5,14 +5,14 @@ class CustomerModifyWorker < QBWC::Worker
     def requests(job)
          Rails.logger.info("This is the start --------- START")
 #         Rails.logger.info("the start of customers ------------ Customers " + customer.name + " " + customer.address)
-        customers = Customer.where("updated_at > created_at")
-            customers.each do |customer|
+#        customers = Customer.where("updated_at > created_at")
+#            customers.each do |customer|
         {
                     :customer_mod_rq => {
                         :customer_mod => {
                             :list_id => "80000079-1576443701",
                             :edit_sequence => "1576443701",
-                            :bill_address => { :addr1 => customer.address, :Addr2 => customer.address2, :City => customer.city, :State => customer.state, :PostalCode => customer.zip}
+                            :bill_address => { :addr1 => "1234", :Addr2 => "010", :City => "Snoho", :State => "WA", :PostalCode => "98104"}
                             }
                         }
                 }
