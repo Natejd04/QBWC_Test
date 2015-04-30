3fcc1fc56e8e2b69f143fdec8ec514df5791bda5
Nate <Natejd04@gmail.com>
2015-04-29 21:55:05 -0700

    trying to pass xml as an array

--- app/workers/customer_modify_worker.rb
+++ app/workers/customer_modify_worker.rb
@@ -3,18 +3,23 @@ require 'qbwc'
 class CustomerModifyWorker < QBWC::Worker
 
     def requests(job)
+         Rails.logger.info("This is the start --------- START")
         {
             :customer_mod_rq => {
-                }
-            }
-            
-#            :customer_mod_rq => {
-#            :customer_mod => {
-#            :list_id => "80000079",
-#            :edit_sequence => "1576443701",
-#            :bill_address => { :addr1 => "1234 Seasme St", :Addr2 => "apt 103", :City => "Seattle", :State => "WA", :PostalCode => "98125"}
+                
+                
+                
 #                }
 #            }
+            
+            :customer_mod => {
+            :list_id => "80000079",
+            :edit_sequence => "1576443701",
+            :bill_address => { :addr1 => "1234 Seasme St", :Addr2 => "apt 103", :City => "Seattle", :State => "WA", :PostalCode => "98125"}
+                }
+            }
+        }
+        
 #                
 #                '<QBXML>
 #                   <QBXMLMsgsRq onError="continueOnError">
@@ -39,27 +44,27 @@ class CustomerModifyWorker < QBWC::Worker
 
     def handle_response(r, session, job, request, data)
         # handle_response will get customers in groups of 100. When this is 0, we're done.
-        Rails.logger.info("This is the start of the customer mod")
+        Rails.logger.info("This is the end of the customer mod")
         
-        "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-            <?qbxml version=\"13.0\"?>
-            <QBXML>
-            <QBXMLMsgsRq onError=\"continueOnError\">
-            <CustomerModRq>
-            <CustomerMod>
-            <ListID>80000079</ListID>
-            <EditSequence>1576443701</EditSequence>
-            <BillAddress>
-            <Addr1>1234 Seasme St</Addr1>
-            <Addr2>Apt 103</Addr2>
-            <City>Seattle</City>
-            <State>WA</State>
-            <PostalCode >98125</PostalCode>
-            </BillAddress>
-            </CustomerModRq>
-            </CustomerMod>
-            </QBXMLMsgsRq>
-                    </QBXML>"
+#        "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+#            <?qbxml version=\"13.0\"?>
+#            <QBXML>
+#            <QBXMLMsgsRq onError=\"continueOnError\">
+#            <CustomerModRq>
+#            <CustomerMod>
+#            <ListID>80000079</ListID>
+#            <EditSequence>1576443701</EditSequence>
+#            <BillAddress>
+#            <Addr1>1234 Seasme St</Addr1>
+#            <Addr2>Apt 103</Addr2>
+#            <City>Seattle</City>
+#            <State>WA</State>
+#            <PostalCode >98125</PostalCode>
+#            </BillAddress>
+#            </CustomerModRq>
+#            </CustomerMod>
+#            </QBXMLMsgsRq>
+#                    </QBXML>"
         
       
     end
