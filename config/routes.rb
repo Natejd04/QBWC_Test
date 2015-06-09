50842c1a43e4c96a03424e1cfab58272981a693e
Nate <Natejd04@gmail.com>
2015-06-09 12:50:59 -0700

    Uninitialized constant ItemsController, when navigating to resources:items

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get 'admin' => 'orders#admin'
   
   resources :items
-  get 'items/index'
+    
 #  delete 'docs/:id'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
