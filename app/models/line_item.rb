2ddf8a056c025d132ad239d33dc4d90f812404c5
Nate <Natejd04@gmail.com>
2015-07-09 17:16:50 -0700

    only accepts line item entries if both fields have data

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,4 @@
 class LineItem < ActiveRecord::Base
     belongs_to :order
+    belongs_to :item
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
     has_many :items
 #    belongs_to :customer, foreign_key: "listid"
     belongs_to :customer
-    accepts_nested_attributes_for :line_items
+    accepts_nested_attributes_for :line_items, :reject_if => :reject_blank_items
     
 #    this is used for the paperclip gem, in order to upload pdfs
     has_attached_file :docs, :url => "/:class/:attachment/:id/:basename.:extension", :path => ":rails_root/public/:class/:attachment/:id/:basename.:extension"
@@ -15,6 +15,13 @@ class Order < ActiveRecord::Base
     before_post_process :docs
     
     #add more of this crazy model, like validation's and such. You know you want to.
+    
+    #This is to eliminate extra entries on Orders
+    def reject_blank_items(attributes)
+      attributes[:product_id].blank? &&
+      attributes[:qty].blank?
+    end
+    
     
 #    This was a test from SO, no succes so far
 #    before_save :destroy_doc?
@@ -33,3 +40,4 @@ class Order < ActiveRecord::Base
 #    end
 #    
 end
+
