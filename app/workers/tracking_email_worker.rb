15fc3358a6affc7abdb0fd1afee064a41137e2da
Nate <Natejd04@gmail.com>
2017-02-14 14:43:02 -0800

    only pull certain templates from within QB

--- app/workers/tracking_email_worker.rb
+++ app/workers/tracking_email_worker.rb
@@ -25,66 +25,69 @@ class TrackingEmailWorker < QBWC::Worker
 
         # We need to confirm that our results are an array           
         
-        if r['invoice_ret'].is_a? Array
+        if r['invoice_ret'].is_a? Array 
 
             r['invoice_ret'].each do |qb_item|
-                item_data = {}
-                item_data[:txn_id] = qb_item['ref_number']
-                item_data[:time_created] = qb_item['time_created']
-                item_data[:txn_date] = qb_item['txn_date']
-                item_data[:name] = qb_item['customer_ref']['full_name']
-                item_data[:template_ref] = qb_item['template_ref']['full_name']
-                
-                if qb_item['memo']
-                    item_data[:memo] = qb_item['memo']
-                end
-                if qb_item['fob']
-                    item_data[:packages] = qb_item['fob']
-                end
-                item_data[:emailed] = false
-
-                # This is the block for the ship to info
-                if qb_item['ship_address']
-                    item_data[:ship1] = qb_item['ship_address']['addr1']
-                    item_data[:ship2] = qb_item['ship_address']['addr2']
-                    item_data[:ship3] = qb_item['ship_address']['addr3']
-                    item_data[:ship4] = qb_item['ship_address']['addr4']
-                    item_data[:ship5] = qb_item['ship_address']['addr5']
-                    item_data[:shipcity] = qb_item['ship_address']['city']
-                    item_data[:shipstate] = qb_item['ship_address']['state']
-                    item_data[:shippostal] = qb_item['ship_address']['postal_code']
-                    item_data[:shipcountry] = qb_item['ship_address']['country']
-                end
-
-                    if qb_item['po_number']                    
-                        email = qb_item['po_number']
-                        if email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-                            item_data[:email] = email     
-                        end
-                    end
-                    if item_data[:email].nil?    
-                        customer = Customer.find_by listid: qb_item['customer_ref']['list_id']
-                        item_data[:email] = customer.email
-                    end
-                
-                item_data[:tracking] = qb_item['other']
+                if qb_item['template_ref']['full_name'] == "* Zing Whls/Consumer Invoice V2"
+                    item_data = {}
+                    item_data[:txn_id] = qb_item['ref_number']
+                    item_data[:time_created] = qb_item['time_created']
+                    item_data[:txn_date] = qb_item['txn_date']
+                    item_data[:name] = qb_item['customer_ref']['full_name']
+                    item_data[:template_ref] = qb_item['template_ref']['full_name']
                     
-                    if qb_item['other']
-                        tracking = qb_item['other']
-                            if tracking =~ /^1Z/
-                                item_data[:ship_method] = "UPS"
-                            elsif tracking =~ /\d{20,22}/
-                                    item_data[:ship_method] = "USPS"
-                            else
-                                item_data[:ship_method] = "FedEx"
+                    if qb_item['memo']
+                        item_data[:memo] = qb_item['memo']
+                    end
+                    if qb_item['fob']
+                        item_data[:packages] = qb_item['fob']
+                    end
+                    item_data[:emailed] = false
+
+                    # This is the block for the ship to info
+                    if qb_item['ship_address']
+                        item_data[:ship1] = qb_item['ship_address']['addr1']
+                        item_data[:ship2] = qb_item['ship_address']['addr2']
+                        item_data[:ship3] = qb_item['ship_address']['addr3']
+                        item_data[:ship4] = qb_item['ship_address']['addr4']
+                        item_data[:ship5] = qb_item['ship_address']['addr5']
+                        item_data[:shipcity] = qb_item['ship_address']['city']
+                        item_data[:shipstate] = qb_item['ship_address']['state']
+                        item_data[:shippostal] = qb_item['ship_address']['postal_code']
+                        item_data[:shipcountry] = qb_item['ship_address']['country']
+                    end
+
+                        if qb_item['po_number']                    
+                            email = qb_item['po_number']
+                            if email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+                                item_data[:email] = email     
                             end
-                    end
+                        end
+                        if item_data[:email].nil?    
+                            customer = Customer.find_by listid: qb_item['customer_ref']['list_id']
+                            item_data[:email] = customer.email
+                        end
                     
-                Tracking.create(item_data)
+                    item_data[:tracking] = qb_item['other']
+                        
+                        if qb_item['other']
+                            tracking = qb_item['other']
+                                if tracking =~ /^1Z/
+                                    item_data[:ship_method] = "UPS"
+                                elsif tracking =~ /\d{20,22}/
+                                        item_data[:ship_method] = "USPS"
+                                else
+                                    item_data[:ship_method] = "FedEx"
+                                end
+                        end
+                        
+                    Tracking.create(item_data)
+                end
             end
+                
 
          # Now we will check to make sure the object isn't empty.   
-        elsif !r['invoice_ret'].blank? 
+        elsif !r['invoice_ret'].blank? && r['invoice_ret']['template_ref']['full_name'] == "* Zing Whls/Consumer Invoice V2"
             item_data = {}
                 item_data[:txn_id] = r['invoice_ret']['ref_number']
                 item_data[:time_created] = r['invoice_ret']['time_created']
