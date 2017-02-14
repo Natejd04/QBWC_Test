5e8c504ec7ba109883314e7df0937bcaab51d5ed
Nate <Natejd04@gmail.com>
2017-02-13 16:07:11 -0800

    had to reduce permissions to re-create admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user
+    before_action :authenticate_user, except:[:new, :create]
     # you can use this to make exceptions ", except:[:new, :create, :edit, :update]"
   
   def index
