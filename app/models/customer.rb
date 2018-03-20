20cfda3feb54a38a3e2e0a178176717e4b40b557
Nate <Natejd04@gmail.com>
2018-03-20 13:16:05 -0700

    updated search for customers, invoices, and sales orders

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,11 @@ class Customer < ActiveRecord::Base
     def self.search(term)
     	if term.match(/[a-zA-Z]/)
   			where('LOWER(name) LIKE :term', term: "%#{term.downcase}%")
-  		else
+  		elsif term.match(/-{1}\d/)
   			# where('LOWER(name) LIKE :term OR LOWER(city) LIKE :term', term: "%#{term.downcase}%")
 			# Order.where('invoice_number LIKE :term')
+      includes(:invoices).where('invoices.c_invoicenumber LIKE :term', term: "#{term}").references(:invoices)
+    else
 			includes(:orders).where('orders.invoice_number LIKE :term', term: "#{term}").references(:orders)
   		end
 	end
--- app/views/dashboard/index.json.jbuilder
+++ app/views/dashboard/index.json.jbuilder
@@ -3,11 +3,11 @@ json.array!(@search) do |book|
 	json.city				book.city
 	json.state     		    book.state
 	json.id					book.id
-	
+	json.orders				book.orders	
+	json.invoices			book.invoices
 	#THIS IS NOT A GREAT SOLUTION
-		json.orders book.orders do |o|
-			json.invoice_number o.invoice_number
-			json.id				o.id
-		end
-	
+	#	json.orders book.orders do |o|
+	#		json.invoice_number o.invoice_number
+	#		json.id				o.id
+	#	end	
 end
