af9d8f749614a64777077e717e4eb8d65656ffc4
Nate <Natejd04@gmail.com>
2018-03-21 16:29:18 -0700

    updaed invoice view, with pagination and table

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,10 @@
 class InvoicesController < ApplicationController
 
+	def index
+		@invoices = Invoice.all.order "id ASC"
+    	@inv1 = Invoice.where("c_date >= ?", 2.months.ago).paginate(:page => params[:page], :per_page => 50).order('c_date ASC')
+	end
+
 	def show
       @order = Invoice.find(params[:id])
   	end
