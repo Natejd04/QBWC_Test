fb8c6ad6f6b7bbfd609ad11cb71826f7ff6b59c2
Nate <Natejd04@gmail.com>
2018-03-23 09:31:52 -0700

    Able to push data into QB

--- app/workers/order_push_worker.rb
+++ app/workers/order_push_worker.rb
@@ -5,10 +5,12 @@ class OrderPushWorker < QBWC::Worker
  def requests(job)
         {
             :sales_order_add_rq => {
-            	:xml_attributes => { "requestID" =>"1", "statusCode" => "0", "statusSeverity" => "Info"},
+            	:xml_attributes => { "requestID" =>"1"},
             	:sales_order_add => {
 	                :customer_ref => {"list_id" => "80000B69-1402348608", "full_name" => "Nate2 Davis"},
-	                
+	                :sales_order_line_add => {
+	                	"desc" => "PoopStick"
+	                }
             	}
         	}
         }
