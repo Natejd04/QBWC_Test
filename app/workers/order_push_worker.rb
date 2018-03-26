5a5d0f650783e818d0fdd831cb811e1ce7b7c0af
Nate <Natejd04@gmail.com>
2018-03-26 15:50:17 -0700

    changes to orders view, as well order_push_worker

--- app/workers/order_push_worker.rb
+++ app/workers/order_push_worker.rb
@@ -2,69 +2,62 @@ require 'qbwc'
 
 class OrderPushWorker < QBWC::Worker
 
+
+
 QBPush = Order.where(qb_process: true)
-
-    order_push = {}
-    s_p = {}
         QBPush.each do |op|
-            op.line_items.each do |li|
-                # Perform the same layout and do a deep deep_merge
-                # Test the each loop hash in the console first
-
-                # example 1
-                s_p = {
-                    :sales_order_add_rq { 
-                        :xml_attributes => {:request_id => "1"},
-                        :sales_order_add => {
-                            :sales_order_line_add =>
-                                :item_ref => {:list_id => li.item.list_id},
-                                :desc => li.description
-                        }
-                    }
-                }
-                # Now merge them
-                s_p = s_p.deep_merge(s_p)
-            end
-
-          order_push = {
-                :sales_order_add_rq => {
-                    :xml_attributes => { "requestID" =>"1"},
-                    :sales_order_add => {
-                        :customer_ref => {"list_id" => QBPush.customer.listid},
-                        :ship_address => {
-                            "addr1" => QBPush.c_ship1,
-                            "addr2" => QBPush.c_ship2,
-                            "addr3" => QBPush.c_ship3,
-                            "city" => QBPush.c_shipcity,
-                            "state" => QBPush.c_shipstate,
-                            "postal_code" => QBPush.c_shippostal,
-                            "country" => QBPush.c_shipcountry 
+            :sales_order_add_rq => {
+                :xml_attributes => { "requestID" =>"1"},
+                :sales_order_add => {
+                    :customer_ref => {"list_id" => op.customer.listid},
+                    :ship_address => {
+                        "addr1" => op.c_ship1,
+                        "addr2" => op.c_ship2,
+                        "addr3" => op.c_ship3,
+                        "city" => op.c_shipcity,
+                        "state" => op.c_shipstate,
+                        "postal_code" => op.c_shippostal,
+                        "country" => op.c_shipcountry 
+                    },
+                     op.line_items.map do |li|
+                        :sales_order_line_add => {
+                            :item_ref => {:list_id => li.item.list_id},
+                            :desc => li.description
                         },
-                            :sales_order_line_add => {
-                                :item_ref => {"list_id" => QBPush.line_items[0].item.list_id},
-                                "desc" => QBPush.line_items[0].description
-                            }
-                        
-                    }
+                    end
+                    :memo => "memo"
                 }
-            }
+            }        
         end
-        # Deep merge the oder_push and s_p
-        o_p.each do |p|
-            push = {}
-            push = s_p.deep_merge(push)
-        end
-
-        # Now merget the string of line item adds, and the order request
-        hash = {}
-        hash = order_push.deep_merge(push)
-
-        #dispaly these results in the Job.
-
-
-
-
+    end
+ 
  def requests(job)    
+    if !QBPush.blank?
+        QBPush.each do |op|
+            :sales_order_add_rq => {
+                :xml_attributes => { "requestID" =>"1"},
+                :sales_order_add => {
+                    :customer_ref => {"list_id" => op.customer.listid},
+                    :ship_address => {
+                        "addr1" => op.c_ship1,
+                        "addr2" => op.c_ship2,
+                        "addr3" => op.c_ship3,
+                        "city" => op.c_shipcity,
+                        "state" => op.c_shipstate,
+                        "postal_code" => op.c_shippostal,
+                        "country" => op.c_shipcountry 
+                    },
+                     op.line_items.map do |li|
+                        :sales_order_line_add => {
+                            :item_ref => {:list_id => li.item.list_id},
+                            :desc => li.description
+                        },
+                    end
+                    :memo => "memo"
+                }
+            }        
+        end
+    end
 
     if QBPush.is_a? Array
             {
