d60e43085cb2505b7c06b57dfe3f86c6e5e3c5fd
Nate <Natejd04@gmail.com>
2018-03-30 11:29:33 -0700

    comment changes:

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+	def create 
+		@order = Order.find(params[:order_id]) 
+		@comment = @order.comments.create(comment_params) 
+		redirect_to order_path(@order) 
+	end 
+
+	private 
+		def comment_params 
+			params.require(:comment). permit(:user_id, :body, :order_id, :customer_id) 
+	end
+
+
+
+
 end
