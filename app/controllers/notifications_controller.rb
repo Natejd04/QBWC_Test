cb95d78a21de5fa5aa65ee1cc902816a8f1d8583
Nate D <Natejd04@gmail.com>
2018-05-01 15:50:12 -0700

    Commented out read all for testing on notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,9 @@ before_action :authenticate_user
 
 	def mark_as_read
 		@notifications = Notification.where(recipient: current_user).unread
+		# For testing I have left this commented out
 		# @notifications.update_all(read_at: Time.zone.now)
+
 		render json: {success: true}
 	end
 end
