bfa3e370ab2a0d764e819a465782f3f242b5a850
Nate D <Natejd04@gmail.com>
2018-06-12 11:46:00 -0700

    account and vendor workers were created, not tested yet.

---
+++ app/workers/account_worker.rb
@@ -0,0 +1,100 @@
+require 'qbwc'
+
+
+class AccountWorker < QBWC::Worker
+
+# Same thing, let's fine out the last time this was pulled, and decide if it's worth it
+    if Log.exists?(worker_name: 'AccountWorker')
+
+        LastUpdate = Log.where(worker_name: 'AccountWorker').order(created_at: :desc).limit(1)
+        LastUpdate = LastUpdate[0][:created_at].strftime("%Y-%m-%d")
+    else
+        # This is preloading data based on no records in the log table
+        LastUpdate = "2000-01-01"
+    
+    end
+
+#    This worker will grab only active items, in the assembly section of QB.
+#    We will use this to populate our item table, so that we can refernce orders and track inventory
+    def requests(job)
+        {
+            :account_query_rq => {
+                :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
+                :max_returned => 500,
+                :from_modified_date => LastUpdate,
+                :to_modified_date => Date.today + (1.0)
+            }
+        }
+    end
+
+    def handle_response(r, session, job, request, data)
+        # handle_response will get customers in groups of 100. When this is 0, we're done.
+        complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+
+        # let's grab all inventory assembly items
+        if r['account_ret'].is_a? Array
+
+            # we will loop through each item and insert it into the Items table.
+            r['account_ret'].each do |qb_account|
+                account_data = {}
+                account_data[:name] = qb_account['full_name']
+                account_data[:description] = qb_account['desc']
+                account_data[:number] = qb_account['account_number']
+                account_data[:type] = qb_account['account_type']
+                account_data[:balance] = qb_account['balance']
+                account_data[:list_id] = qb_account['list_id']
+                account_data[:edit_sq] = qb_account['edit_sequence']
+                account_data[:active] = qb_account['is_active']
+                account_data[:qb_created] = qb_account['time_created']
+                account_data[:qb_modified] = qb_account['time_modified']
+
+
+                if qb_account['currency_ref']                    
+                    account_data[:currency] = qb_account['currency_ref']['full_name']
+                end
+               
+                if Account.exists?(list_id: qb_account['list_id'])
+                    accountupdate = Account.find_by(list_id: qb_account['list_id'])
+                    # before updating, lets find out if it's neccessary by filtering by modified
+                    if accountupdate.edit_sq != qb_account['edit_sequence']
+                        accountupdate.update(account_data)
+                    end
+                else
+                    Account.create(account_data)
+                end
+            end
+
+        # This is if there is only 1 item update
+        elsif !r['account_ret'].blank? 
+            qb_account = r['account_ret']
+            account_data = {}
+                account_data[:name] = qb_account['full_name']
+                account_data[:description] = qb_account['desc']
+                account_data[:number] = qb_account['account_number']
+                account_data[:type] = qb_account['account_type']
+                account_data[:balance] = qb_account['balance']
+                account_data[:list_id] = qb_account['list_id']
+                account_data[:edit_sq] = qb_account['edit_sequence']
+                account_data[:active] = qb_account['is_active']
+                account_data[:qb_created] = qb_account['time_created']
+                account_data[:qb_modified] = qb_account['time_modified']
+
+
+                if qb_account['currency_ref']                    
+                    account_data[:currency] = qb_account['currency_ref']['full_name']
+                end
+               
+                if Account.exists?(list_id: qb_account['list_id'])
+                    accountupdate = Account.find_by(list_id: qb_account['list_id'])
+                    # before updating, lets find out if it's neccessary by filtering by modified
+                    if accountupdate.edit_sq != qb_account['edit_sequence']
+                        accountupdate.update(account_data)
+                    end
+                else
+                    Account.create(account_data)
+                end
+        # End of the Assembly items
+      Log.create(worker_name: "AccountWorker")
+
+    end
+end
---
+++ app/workers/vendor_worker.rb
@@ -0,0 +1,131 @@
+require 'qbwc'
+
+class VendorWorker < QBWC::Worker
+
+    # Pre-load all customer data, only if no data exists in the Log table.
+    # If data exists in the Log table, we take the last pull date as a sort filter
+    # We will limit this to 1, the most recent entry
+    if Log.exists?(worker_name: 'VendorWorker')
+
+        LastUpdate = Log.where(worker_name: 'VendorWorker').order(created_at: :desc).limit(1)
+        LastUpdate = LastUpdate[0][:created_at].strftime("%Y-%m-%d")
+    else
+        # This is preloading data based on no records in the log table
+        LastUpdate = "2014-01-01"
+    
+    end
+
+    # Modified this worker, you can use this to load all data, and have it continually run.
+    def requests(job)
+        {
+            :vendor_query_rq => {
+                :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
+                :max_returned => 1000,
+                :from_modified_date => LastUpdate,
+                :to_modified_date => Date.today + (1.0),
+                :is_active => true
+            }
+        }
+    end
+    
+
+    def handle_response(r, session, job, request, data)
+        # handle_response will get customers in groups of 100. When this is 0, we're done.
+        complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+
+        # if no customer updates occured we skip this.
+        # if r['vendor_ret']
+        if r['vendor_ret'].is_a? Array
+    #        We will then loop through each customer and create records.
+            r['vendor_ret'].each do |qb_ven|
+                vendor_data = {}
+                vendor_data[:list_id] = qb_ven['list_id']
+                vendor_data[:name] = qb_ven['name']
+                vendor_data[:edit_sq] = qb_ven['edit_sequence']
+                vendor_data[:qb_created] = qb_ven['time_created']
+                vendor_data[:qb_modified] = qb_ven['time_modified']
+                vendor_data[:balance] = qb_ven['balance']
+
+                if qb_ven['vendor_address']
+                    vendor_data[:address1] = qb_ven['vendor_address']['addr1']
+                    vendor_data[:address2] = qb_ven['vendor_address']['addr2']
+                    vendor_data[:address3] = qb_ven['vendor_address']['addr3']
+                    vendor_data[:address4] = qb_ven['vendor_address']['addr4']
+                    vendor_data[:address5] = qb_ven['vendor_address']['addr5']
+                    vendor_data[:city] = qb_ven['vendor_address']['city']
+                    vendor_data[:state] = qb_ven['vendor_address']['state']
+                    vendor_data[:zip] = qb_ven['vendor_address']['postal_code']
+                    vendor_data[:country] = qb_ven['vendor_address']['country']
+                end
+
+                if qb_ven['additional_contact_ref']
+                    vendor_data[:contact_name] = qb_ven['additional_contact_ref']['contact_name']
+                    vendor_data[:contact_value] = qb_ven['additional_contact_ref']['contact_value']
+                end
+
+                if Vendor.exists?(list_id: qb_ven['list_id'])
+                vendorid = Vendor.find_by(list_id: vendor_data[:list_id])
+                    
+                    # We want to confirm that it's neccessary to update this record first.
+                    if vendorid.edit_sq != qb_ven['edit_sequence']
+                        vendorid.update(vendor_data)
+                    end
+                
+                else
+                   
+                    # the customer didn't exists so we will create
+                    Vendor.create(vendor_data)
+                
+                end
+            end
+            # let's record that this worker was ran, so that it's timestamped in logs
+            # Moved the log creating to be within the handle response incase the response errors, I don't want a log.
+            Log.create(worker_name: "VendorWorker")
+
+                # Now we will check to make sure the object isn't empty.   
+        elsif !r['vendor_ret'].blank? 
+                 vendor_data = {}
+                vendor_data[:list_id] = qb_ven['list_id']
+                vendor_data[:name] = qb_ven['name']
+                vendor_data[:edit_sq] = qb_ven['edit_sequence']
+                vendor_data[:qb_created] = qb_ven['time_created']
+                vendor_data[:qb_modified] = qb_ven['time_modified']
+                vendor_data[:balance] = qb_ven['balance']
+
+                if qb_ven['vendor_address']
+                    vendor_data[:address1] = qb_ven['vendor_address']['addr1']
+                    vendor_data[:address2] = qb_ven['vendor_address']['addr2']
+                    vendor_data[:address3] = qb_ven['vendor_address']['addr3']
+                    vendor_data[:address4] = qb_ven['vendor_address']['addr4']
+                    vendor_data[:address5] = qb_ven['vendor_address']['addr5']
+                    vendor_data[:city] = qb_ven['vendor_address']['city']
+                    vendor_data[:state] = qb_ven['vendor_address']['state']
+                    vendor_data[:zip] = qb_ven['vendor_address']['postal_code']
+                    vendor_data[:country] = qb_ven['vendor_address']['country']
+                end
+
+                if qb_ven['additional_contact_ref']
+                    vendor_data[:contact_name] = qb_ven['additional_contact_ref']['contact_name']
+                    vendor_data[:contact_value] = qb_ven['additional_contact_ref']['contact_value']
+                end
+
+                if Vendor.exists?(list_id: qb_ven['list_id'])
+                vendorid = Vendor.find_by(list_id: vendor_data[:list_id])
+                    
+                    # We want to confirm that it's neccessary to update this record first.
+                    if vendorid.edit_sq != qb_ven['edit_sequence']
+                        vendorid.update(vendor_data)
+                    end
+                
+                else
+                   
+                    # the customer didn't exists so we will create
+                    Vendor.create(vendor_data)
+                
+                end
+            # let's record that this worker was ran, so that it's timestamped in logs
+            # Moved the log create to be within the handle response incase the response has errors and I don't want it to log.
+            Log.create(worker_name: "VendorWorker")
+        end
+    end
+end
