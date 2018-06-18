417f0f7a7635f0cdd75f9491295db04c89176c28
Nate D <Natejd04@gmail.com>
2018-06-18 13:28:42 -0700

    added sales receipt controller and helpers

---
+++ app/helpers/sales_receipts_helper.rb
@@ -0,0 +1,2 @@
+module SalesReceiptsHelper
+end
---
+++ test/controllers/sales_receipts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SalesReceiptsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
