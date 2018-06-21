e0d27bf59e31dc829538d343abfd08cbcfadf0c2
Nate D <Natejd04@gmail.com>
2018-06-21 11:14:17 -0700

    using custom controllers with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 #  include SessionsHelper
  
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-    def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:phone])
-    end
-
-
-
     # #establish the sign in constants
     # def sign_in(user)
     #     session[:user_id] = user.id
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users, controllers: { registrations: 'users/registrations'}, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'order/index'
 
 #  get 'users/new'
