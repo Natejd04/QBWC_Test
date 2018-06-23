9b858e5a42bbde25d53105a88bec42becd023307
Nate D <Natejd04@gmail.com>
2018-06-22 17:25:50 -0700

    Successful bootup and load, working on QBWC. Should have rights now

--- app/controllers/qbwc_controller.rb
+++ app/controllers/qbwc_controller.rb
@@ -1,6 +1,6 @@
 class QbwcController < ApplicationController  
-  before_action :authenticate_user!, except:[:_generate_wsdl]
-  before_action :admin_only, except:[:_generate_wsdl]
+  before_action :authenticate_user!, except:[:_generate_wsdl, :action]
+  # before_action :admin_only, except:[:_generate_wsdl]
   include QBWC::Controller
 
 end
