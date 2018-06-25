b91429871a6e533a7addb772ed659c07225295ec
Nate D <Natejd04@gmail.com>
2018-06-25 12:10:54 -0700

    giving users ability to edit name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,12 +44,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :avatar, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone, :avatar, :role, :name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :avatar, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :avatar, :role, :name])
   end
 
   def active_for_authentication?
