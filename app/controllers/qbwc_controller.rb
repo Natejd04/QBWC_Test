c40569c5466a5f251d6d830b024937acf7c83fb6
Nate D <Natejd04@gmail.com>
2018-06-25 12:51:34 -0700

    first connection without exceptions

--- app/controllers/qbwc_controller.rb
+++ app/controllers/qbwc_controller.rb
@@ -1,5 +1,5 @@
 class QbwcController < ApplicationController  
-  before_action :authenticate_user!, except:[:_generate_wsdl, :action]
+  # before_action :authenticate_user!, except:[:_generate_wsdl, :action]
   # before_action :admin_only, except:[:_generate_wsdl]
   include QBWC::Controller
 
