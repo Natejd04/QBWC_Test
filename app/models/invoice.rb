bbd4803c9c6e250f67bc57392698ccdebedbc666
Nate D <Natejd04@gmail.com>
2018-07-02 09:40:11 -0700

    sales order changes

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,8 @@
 class Invoice < ActiveRecord::Base
-	has_many :line_items, :dependent => :destroy, foreign_key: "order_id"
-    has_many :items, through: :line_items, foreign_key: "order_id"
+	has_many :line_items, :dependent => :destroy, foreign_key: "invoice_id"
+    has_many :items, through: :line_items, foreign_key: "invoice_id"
     has_many :sites, through: :line_items, foreign_key: "site_id"
-    has_many :comments, :dependent => :destroy, foreign_key: "order_id"
+    has_many :comments, :dependent => :destroy, foreign_key: "invoice_id"
     belongs_to :customer
 
     default_scope {where(:deleted => nil)}
