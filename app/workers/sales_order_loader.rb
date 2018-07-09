f4fff07730f242204dbba83ab674679973917301
Nate D <Natejd04@gmail.com>
2018-07-09 13:55:30 -0700

    corrected nil class notification error in sales_order_loader. I also fixed Amazon, so that it shows Pending Confirm.

--- app/workers/sales_order_loader.rb
+++ app/workers/sales_order_loader.rb
@@ -132,15 +132,15 @@ class SalesOrderLoader < QBWC::Worker
                     admin = User.where(role: "admin").select("name, email, role, id")
                     combo = User.where("role = ? or role = ?", "admin", "sales").select("name, email, role, id")
                     if qb_inv['class_ref'].nil?
-                    binding.pry 
-                        if qb_inv['class_ref']['full_name'] == "Distributor Class"  || qb_inv['class_ref']['full_name'] == "Amazon VC"
-                            combo.each do |user|
-                                Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
-                            end
-                        else
-                            admin.each do |user|
-                                Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
-                            end
+                        qb_inv['class_ref']['full_name'] = nil
+                    end
+                    if qb_inv['class_ref']['full_name'] == "Distributor Class"  || qb_inv['class_ref']['full_name'] == "Amazon VC"
+                        combo.each do |user|
+                            Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
+                        end
+                    else
+                        admin.each do |user|
+                            Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
                         end
                     end
                 end
@@ -345,20 +345,21 @@ class SalesOrderLoader < QBWC::Worker
             else
                 Order.create(invoice_data)
                 # if InitialLoad == false
-                    inv_created = Order.find_by(txn_id: invoice_data[:txn_id])
-                    admin = User.where(role: "admin").select("name, email, role, id")
-                    combo = User.where("role = ? or role = ?", "admin", "sales").select("name, email, role, id")
-                    # if qb_inv['class_ref']
-                        if qb_inv['class_ref']['full_name'] == "Distributor Class"  || qb_inv['class_ref']['full_name'] == "Amazon VC"
-                            combo.each do |user|
-                                Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
-                            end
-                        else
-                            admin.each do |user|
-                                Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
-                            end
-                        end
-                # end
+                inv_created = Order.find_by(txn_id: invoice_data[:txn_id])
+                admin = User.where(role: "admin").select("name, email, role, id")
+                combo = User.where("role = ? or role = ?", "admin", "sales").select("name, email, role, id")
+                if qb_inv['class_ref'].nil?
+                    qb_inv['class_ref']['full_name'] = nil
+                end
+                if qb_inv['class_ref']['full_name'] == "Distributor Class"  || qb_inv['class_ref']['full_name'] == "Amazon VC"
+                    combo.each do |user|
+                        Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
+                    end
+                else
+                    admin.each do |user|
+                        Notification.create(recipient_id: user.id, action: "posted", notifiable: inv_created)
+                    end
+                end
             end
         
         # ----------------> Start Line Item
