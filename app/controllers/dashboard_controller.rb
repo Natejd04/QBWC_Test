965ef1990a314f476bcafc92ff51c94c5a3ba3a1
Nate D <Natejd04@gmail.com>
2018-07-09 13:59:56 -0700

    changed default ordering

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -86,11 +86,11 @@ class DashboardController < ApplicationController
     private
 
     def sort_column
-    	%w[c_ship c_date c_total c_name].include?(params[:sort]) ? params[:sort] : "c_ship"
+    	%w[c_ship c_date c_total c_name].include?(params[:sort]) ? params[:sort] : "c_date"
     end
 
     def sort_direction
-    	%w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    	%w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 
     def classed_remove
