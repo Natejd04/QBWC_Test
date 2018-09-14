fd1097392f4451d5ded3801b0121510f645b7c84
Nate <Natejd04@gmail.com>
2018-09-13 17:36:18 -0700

    migrating items from the workers to the helper. Moved Log function and worker date ranges

--- app/workers/concerns/qbwc_helper.rb
+++ app/workers/concerns/qbwc_helper.rb
@@ -8,7 +8,18 @@ module QbwcHelper
 	      	end
 	    else
 	      # This is preloading data based on no records in the log table
+	      # This is arbitrary and a system for loading in batches from the start should be implemented.
 	      3.month.ago.strftime("%Y-%m-%d")
 	    end
+	end
+
+	def qbwc_log_create(worker, stat, msg)
+		if stat == "none"
+			Log.create(worker_name: WorkerName, status: "No Changes")
+		elsif stat == "updates"
+			Log.create(worker_name: WorkerName, status: stat, log_msg: msg + "record(s) were updated")
+		elsif stat == "completed"
+			Log.create(worker_name: WorkerName, status: "Completed")
+		end
 	end
 end
--- app/workers/list_delete_worker.rb
+++ app/workers/list_delete_worker.rb
@@ -4,7 +4,8 @@ class ListDeleteWorker < QBWC::Worker
     extend QbwcHelper
 
 # Same thing, let's fine out the last time this was pulled, and decide if it's worth it
-    LastUpdate = qbwc_log_init("ListDeleteWorker")
+    WorkerName = "ListDeleteWorker"
+    # LastUpdate = qbwc_log_init(WorkerName)
 
 #    This worker will grab only active items, in the assembly section of QB.
 #    We will use this to populate our item table, so that we can refernce orders and track inventory
@@ -13,7 +14,7 @@ class ListDeleteWorker < QBWC::Worker
             :list_deleted_query_rq => {
                 :xml_attributes => { "requestID" =>"1"},
                 :list_del_type => ["Account", "Customer", "InventorySite", "ItemDiscount", "ItemFixedAsset", "ItemGroup", "ItemInventory", "ItemInventoryAssembly", "ItemNonInventory", "ItemOtherCharge", "ItemPayment", "ItemService", "ItemSubtotal", "Vendor"],
-                :deleted_date_range_filter => {"from_deleted_date" => LastUpdate, "to_deleted_date" => Date.today + (1.0)}
+                :deleted_date_range_filter => {"from_deleted_date" => qbwc_log_init(WorkerName), "to_deleted_date" => Date.today + (1.0)}
             }
         }
     end
@@ -23,15 +24,12 @@ class ListDeleteWorker < QBWC::Worker
         complete = r['xml_attributes']['iteratorRemainingCount'] == '0'        
         # let's grab all inventory assembly items
         if r['list_deleted_ret'].nil? 
-            # if it's true then we lay down a log update, saying not updated"
-            # Need to add a column to log in order to state when the last one was run
-            # UNLESS there is no trues, it goes with the last empty valued column
-            Log.create(worker_name: "ListDeleteWorker", status: "No Changes")
+            # This will log if the data returned was empty and no updates occured, but it did run.
+            qbwc_log_create(WorkerName, "none", nil)            
         else
             # Data was fetched, and will execute
             # We need a way to say that there wasn't an error, and if so...mark complete.
-            if r['list_deleted_ret'].is_a? Array
-                i = 0
+            if r['list_deleted_ret'].is_a? Array                
                 # we will loop through each item and insert it into the Items table.
                 r['list_deleted_ret'].each_with_index do |qb_data, index|
                     if qb_data['list_del_type']
@@ -59,7 +57,8 @@ class ListDeleteWorker < QBWC::Worker
                             if table.exists?(list_id: qb_data['list_id'])
                                 list_element = table.find_by(list_id: qb_data['list_id'])
                                 list_element.update(delete_data)
-                                Log.create(worker_name: "ListDeleteWorker", status: "Updates", log_msg: "#{index} records were updated.")
+                                # This will record how many updates were made.
+                                qbwc_log_create(WorkerName, "updates", index)
                             end
                         end
                     end
@@ -96,14 +95,14 @@ class ListDeleteWorker < QBWC::Worker
                         if table.exists?(list_id: qb_data['list_id'])
                             list_element = table.find_by(list_id: qb_data['list_id'])
                             list_element.update(delete_data)
-                            Log.create(worker_name: "ListDeleteWorker", status: "Updates", log_msg: "One record was updated.")
+                            qbwc_log_create(WorkerName, "updates", "1")                            
                         end
                     end
                 end
             # End of the Accounts
             end
         # Only if complete, put the complete one.
-        Log.create(worker_name: "ListDeleteWorker", status: "Complete")
+        qbwc_log_create(WorkerName, "complete", nil)
         end #closing of the if log statement
 
         
