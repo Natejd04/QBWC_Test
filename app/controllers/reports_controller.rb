3cb2bc95c2a187a781f548ae83f5a7ee6334fef0
Nate D <Natejd04@gmail.com>
2018-10-15 14:24:20 -0700

    added Amazon as a customer group, and made minor tweaks to the layout of the report section

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,5 +3,18 @@ class ReportsController < ApplicationController
 	def index
 
 	end
+
+	def show
+		case params[:id]
+		when "customer_sales"
+			render_customer_sales
+		else
+			redirect_to reports_path, alert: "This report doesn't exist."
+		end
+	end
+
+	def render_customer_sales
+		render 'reports/customer_sales'
+	end
 	
 end
