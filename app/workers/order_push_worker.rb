f7ae38c9410c4092db438ab253792f2f03866f3c
Nate Davis <natejd04@gmail.com>
2019-08-11 10:28:07 -0700

    avoiding initialized constants

--- app/workers/order_push_worker.rb
+++ app/workers/order_push_worker.rb
@@ -1,6 +1,6 @@
 require 'qbwc'
 require 'concerns/qbwc_helper'
-#QBsend = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
+#qb_send = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
 class OrderPushWorker < QBWC::Worker
     include QbwcHelper
 
@@ -10,11 +10,11 @@ class OrderPushWorker < QBWC::Worker
     
     #Was asked to send Amazon DF orders direct to invoice. This naming convention is misleading
     WorkerName = "OrderPushWorker"
-    QBsend = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
+    qb_send = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
     def requests(job)    
     
-        if !QBsend.blank?
-            QBsend.map do |op|
+        if !qb_send.blank?
+            qb_send.map do |op|
                     { :invoice_add_rq => {
                         :xml_attributes => { "requestID" =>"1"},
                         :invoice_add => {
@@ -48,32 +48,32 @@ class OrderPushWorker < QBWC::Worker
                 }
             end      
 
-        elsif QBsend.is_a? Array
+        elsif qb_send.is_a? Array
             {
                 :invoice_add_rq => {
                     :xml_attributes => { "requestID" =>"1"},
                     :invoice_add => {
-                        :customer_ref => {"list_id" => QBsend.customer.list_id},
-                        :class_ref => {"full_name" => QBsend.c_class},
-                        :txn_date => QBsend.c_date,
+                        :customer_ref => {"list_id" => qb_send.customer.list_id},
+                        :class_ref => {"full_name" => qb_send.c_class},
+                        :txn_date => qb_send.c_date,
                         :ship_address => {
-                            "addr1" => QBsend.c_ship1,
-                            "addr2" => QBsend.c_ship2,
-                            "addr3" => QBsend.c_ship3,                            
-                            "city" => QBsend.c_shipcity,
-                            "state" => QBsend.c_shipstate,
-                            "postal_code" => QBsend.c_shippostal,
-                            "country" => QBsend.c_shipcountry 
+                            "addr1" => qb_send.c_ship1,
+                            "addr2" => qb_send.c_ship2,
+                            "addr3" => qb_send.c_ship3,                            
+                            "city" => qb_send.c_shipcity,
+                            "state" => qb_send.c_shipstate,
+                            "postal_code" => qb_send.c_shippostal,
+                            "country" => qb_send.c_shipcountry 
                         },
-                        :po_number => QBsend.c_po,
+                        :po_number => qb_send.c_po,
                         :due_date => op.c_ship.strftime("%Y-%m-%d"),
                         :ship_date => op.c_ship.strftime("%Y-%m-%d"),
-                        :invoice_line_add => QBsend.line_items.map do |li|
+                        :invoice_line_add => qb_send.line_items.map do |li|
                             {
                                 :item_ref => {:list_id => li.item.list_id},
                                 :desc => li.description,
                                 :quantity => li.qty,
-                                :class_ref => {:full_name => QBsend.c_class},
+                                :class_ref => {:full_name => qb_send.c_class},
                                 :amount => '%.2f' % li.amount,
                                 :inventory_site_ref => {:list_id => li.site.list_id}
                             }
@@ -155,7 +155,7 @@ class OrderPushWorker < QBWC::Worker
         # Log.create(worker_name: "OrderPushWorker")
 
     end
-QBsend = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
+qb_send = Order.where(send_to_qb: true, qb_process: true, qb_sent_time: nil)
 end
 
 
